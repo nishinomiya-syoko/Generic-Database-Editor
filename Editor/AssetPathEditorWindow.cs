02a3d739890e20f397467041133c334a1d8c5ade
西宫硝子 <wuyuhaomail@163.com>
2026-06-13 19:17:22 +0800

    删除自动mac 不可用代码

--- Editor/AssetPathEditorWindow.cs
+++ Editor/AssetPathEditorWindow.cs
@@ -663,8 +663,9 @@ namespace NiShiMiYa.GenericEditor
                         ws.Cells[5 + i, 3].Value = item.assetPath;
                         ws.Cells[5 + i, 4].Value = item.assetName;
                     }
-
-                    ws.Cells[ws.Dimension.Address].AutoFitColumns();
+                    
+                    //mac不可用
+                    // ws.Cells[ws.Dimension.Address].AutoFitColumns();
                     package.SaveAs(new FileInfo(excelPath));
                 }
 
